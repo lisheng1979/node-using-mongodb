b3f8592c589f727fd8d7d24667885d9929294be9
lisheng1979 <skyeyes@263.net>
2020-06-07 00:17:17 -0700

    Completed module

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,14 @@ import { addnewProduct,
 
 const routes = (app) => {
     app.route('/products')
+        //get multiple products
         .get(getProducts)   
-
+        
+        //Post endpoint
         .post(addnewProduct);  
     
     app.route('/products/:ProductID')
+        //get a specific product with an ID
         .get(getProductWithID);
     
 }
