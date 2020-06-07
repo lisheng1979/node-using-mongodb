4413b629fca4ba3e81da51143e861873bba37943
lisheng1979 <skyeyes@263.net>
2020-06-06 23:13:49 -0700

    Completed module

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -7,7 +7,7 @@ export const addnewProduct = (req, res) => {
     let newProduct = new Product(req.body);
 
     newProduct.save((err, Product) => {
-        if(err) {
+        if( err ) {
             res.send(err);
         }
         res.json(Product);
